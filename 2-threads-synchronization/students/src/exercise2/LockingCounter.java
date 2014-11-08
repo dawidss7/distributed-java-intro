c745388c6a7ec2ff473f535f488e598e9b46c1d8
Dawid Jewko <dawid.jewko@gmail.com>
2014-11-08 11:11:06 +0100

    init project

--- 2-threads-synchronization/students/src/exercise2/LockingCounter.java
+++ 2-threads-synchronization/students/src/exercise2/LockingCounter.java
@@ -7,13 +7,16 @@ import java.util.concurrent.locks.ReentrantLock;
 public class LockingCounter implements Counter {
     private long counter = 0;
     public final ReentrantLock lock = new ReentrantLock();
+    public final ReentrantLock lock2 = new ReentrantLock();
 
     @Override
     public void increment() {
         lock.lock();
+        lock2.lock();
         try {
             counter++;
         } finally {
+            lock2.unlock();
             lock.unlock();
         }
 
